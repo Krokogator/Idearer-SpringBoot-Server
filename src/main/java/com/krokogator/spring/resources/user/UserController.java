d798152a104f873fc7779b65b9989d921c4d4c1f
Krokogator <michal26262@gmail.com>
2018-09-21 20:08:40 +0200

    Attempt on custom requestBody in User

--- src/main/java/com/krokogator/spring/resources/user/UserController.java
+++ src/main/java/com/krokogator/spring/resources/user/UserController.java
@@ -1,8 +1,9 @@
 package com.krokogator.spring.resources.user;
 
-import com.krokogator.spring.resources.user.validation.GetUser;
 import com.krokogator.spring.resources.user.validation.PostUser;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -20,6 +21,9 @@ public class UserController {
     UserService userService;
 
     @PostMapping
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "user", required = true, dataType = "UserPost", paramType = "body")
+    })
     public User registerUser(@Validated ({PostUser.class, Default.class}) @RequestBody User user){
         return userService.saveUser(user);
     }
