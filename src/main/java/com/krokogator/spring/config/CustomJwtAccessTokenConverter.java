69957e0310f934777bf7ea6154e91093957949af
Krokogator <michal26262@gmail.com>
2018-10-07 11:36:38 +0200

    Removed userId from /oauth/token response body (encoded into access token)

--- src/main/java/com/krokogator/spring/config/CustomJwtAccessTokenConverter.java
+++ src/main/java/com/krokogator/spring/config/CustomJwtAccessTokenConverter.java
@@ -5,8 +5,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.oauth2.common.DefaultOAuth2AccessToken;
 import org.springframework.security.oauth2.common.OAuth2AccessToken;
 import org.springframework.security.oauth2.provider.OAuth2Authentication;
+import org.springframework.security.oauth2.provider.token.DefaultAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 
+import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
@@ -26,7 +28,11 @@ public class CustomJwtAccessTokenConverter extends JwtAccessTokenConverter {
         DefaultOAuth2AccessToken customAccessToken = new DefaultOAuth2AccessToken(accessToken);
         customAccessToken.setAdditionalInformation(info);
 
-        return super.enhance(customAccessToken, authentication);
+        accessToken = super.enhance(customAccessToken, authentication);
+        ((DefaultOAuth2AccessToken) accessToken).setAdditionalInformation(new HashMap<>());
+
+        return accessToken;
+
     }
 
     @Override
