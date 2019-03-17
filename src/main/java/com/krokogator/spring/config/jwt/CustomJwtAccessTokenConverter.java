97cdfac43cc65fbf62c74c857ba46187d4a5b2b5
Krokogator <michal26262@gmail.com>
2019-03-17 19:32:10 +0100

    Fix refresh token endpoint

--- src/main/java/com/krokogator/spring/config/jwt/CustomJwtAccessTokenConverter.java
+++ src/main/java/com/krokogator/spring/config/jwt/CustomJwtAccessTokenConverter.java
@@ -18,17 +18,25 @@ public class CustomJwtAccessTokenConverter extends JwtAccessTokenConverter {
 
     @Override
     public OAuth2AccessToken enhance(OAuth2AccessToken accessToken, OAuth2Authentication authentication) {
-        Map<String, Object> info = new LinkedHashMap<>(accessToken.getAdditionalInformation());
 
-        //Here put additional DATA
-        SecureUser authenticatedUser = (SecureUser) authentication.getPrincipal();
-        info.put("userId", authenticatedUser.getId());
-        info.put("userName", authenticatedUser.getUsername());
-        DefaultOAuth2AccessToken customAccessToken = new DefaultOAuth2AccessToken(accessToken);
-        customAccessToken.setAdditionalInformation(info);
 
-        //Convert into accessToken
-        accessToken = super.enhance(customAccessToken, authentication);
+        try {
+            DefaultOAuth2AccessToken customAccessToken = new DefaultOAuth2AccessToken(accessToken);
+
+            Map<String, Object> info = new LinkedHashMap<>(accessToken.getAdditionalInformation());
+
+            //Here put additional DATA
+            SecureUser authenticatedUser = (SecureUser) authentication.getPrincipal();
+            info.put("userId", authenticatedUser.getId());
+            info.put("userName", authenticatedUser.getUsername());
+            customAccessToken.setAdditionalInformation(info);
+            //Convert into accessToken
+            accessToken = super.enhance(customAccessToken, authentication);
+        } catch (Exception e) {
+            accessToken = super.enhance(accessToken, authentication);
+        }
+
+
 
         //Clear additional DATA from response body
         //((DefaultOAuth2AccessToken) accessToken).setAdditionalInformation(new HashMap<>());
@@ -40,8 +48,13 @@ public class CustomJwtAccessTokenConverter extends JwtAccessTokenConverter {
     @Override
     public OAuth2Authentication extractAuthentication(Map<String, ?> map) {
         OAuth2Authentication authentication = super.extractAuthentication(map);
-        IdAwareOAuth2Request idAwareOAuth2Request = new IdAwareOAuth2Request(authentication.getOAuth2Request());
-        idAwareOAuth2Request.setUserId(Long.valueOf((Integer)map.get("userId")));
-        return new OAuth2Authentication(idAwareOAuth2Request, authentication.getUserAuthentication());
+        try {
+            IdAwareOAuth2Request idAwareOAuth2Request = new IdAwareOAuth2Request(authentication.getOAuth2Request());
+            idAwareOAuth2Request.setUserId(Long.valueOf((Integer) map.get("userId")));
+            authentication = new OAuth2Authentication(idAwareOAuth2Request, authentication.getUserAuthentication());
+        } catch (Exception e) {
+        }
+
+        return authentication;
     }
 }
